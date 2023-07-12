a3ab81768908c8193d5b7a9ece6f0640ed14816a
TomazDimas <tomazdimas2001@gmail.com>
2023-07-12 15:24:43 -0300

    feat: Construção inicial do header

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  AiOutlineSearch,
+  AiOutlineUnorderedList,
+  AiOutlineUser,
+  AiOutlineCaretDown,
+  AiOutlineShoppingCart,
+} from 'react-icons/ai';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header>
+      <div className={ styles.container }>
+        <a href="/">
+          <img
+            src="https://ibassets.com.br/ib.store.image.medium/m-0cd410fa3a4844acb4294a0edfea3822.png"
+            alt="Supermercado Modelo"
+          />
+          <p>Supermercado Modelo</p>
+        </a>
+        <form>
+          <input type="text" />
+          <AiOutlineSearch />
+        </form>
+        <a href="/">
+          <AiOutlineUnorderedList />
+          Listas
+        </a>
+        <button type="button">
+          <AiOutlineUser />
+          <p>Minha conta</p>
+          <AiOutlineCaretDown />
+        </button>
+        <button type="button">
+          <AiOutlineShoppingCart />
+          <p>Minha conta</p>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
