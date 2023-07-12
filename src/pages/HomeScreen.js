48818f4c2e863d5cf7eac65259da58c507579fac
TomazDimas <tomazdimas2001@gmail.com>
2023-07-12 14:55:46 -0300

    feat: Configurações iniciais de código

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function HomeScreen() {
+  return (
+    <header>
+      <div>
+        <a href="/">
+          <img src="https://ibassets.com.br/ib.store.image.medium/m-0cd410fa3a4844acb4294a0edfea3822.png" alt="Supermercado Modelo" />
+          <p>Supermercado Modelo</p>
+        </a>
+        <form>
+          <input
+            type="text"
+          />
+          <img src="https://e7.pngegg.com/pngimages/623/772/png-clipart-magnifying-glass-icon-black-magnifier.png" alt="Busca" />
+        </form>
+      </div>
+    </header>
+  );
+}
+
+export default HomeScreen;
