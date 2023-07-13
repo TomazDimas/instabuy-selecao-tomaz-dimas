22881301a5754b1c6c1c4d0700a3bad829602f91
TomazDimas <tomazdimas2001@gmail.com>
2023-07-13 00:04:37 -0300

    feat: Adaptação dos arquivos app e index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Header from './components/Header/Header';
 import HomeScreen from './pages/HomeScreen';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <HomeScreen />
     </div>
   );
