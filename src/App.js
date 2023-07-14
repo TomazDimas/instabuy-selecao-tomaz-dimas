aac0445614099927bb6c4a76e68df8e65ed29433
TomazDimas <tomazdimas2001@gmail.com>
2023-07-14 19:25:19 -0300

    feat: Criação da barra de navegação

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
 import HomeScreen from './pages/HomeScreem/HomeScreen';
 import ProductScreen from './pages/ProductScreen/ProductScreen';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
       <Routes>
         <Route exact path="/" element={ <HomeScreen /> } />
         <Route path="product/:slug" element={ <ProductScreen /> } />
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import {
+  AiOutlineHome,
+  AiOutlineAppstore,
+  AiOutlineTags,
+} from 'react-icons/ai';
+import { GiBarbecue } from 'react-icons/gi';
+import { TbMeat } from 'react-icons/tb';
+import { BiCheese, BiDrink } from 'react-icons/bi';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 function Navegation() {
   return (
-    <nav>
-      <li>Início</li>
-      <li>Todas Categorias</li>
-      <li>Ofertas</li>
+    <nav className="navbar-container">
+      <NavLink to="/">
+        <AiOutlineHome />
+        Home
+      </NavLink>
+      <NavLink to="/categories">
+        <AiOutlineAppstore />
+        Todas Categorias
+      </NavLink>
+      <NavLink to="/offers">
+        <AiOutlineTags />
+        Ofertas
+      </NavLink>
+      <NavLink to="/barbecue">
+        <GiBarbecue />
+        Kit Churrasco
+      </NavLink>
+      <NavLink to="/meats">
+        <TbMeat />
+        Açougue/ Aves/ Peixaria
+      </NavLink>
+      <NavLink to="/dairy">
+        <BiCheese />
+        Laticínios
+      </NavLink>
+      <NavLink to="/drinks">
+        <BiDrink />
+        Bebidas Alcoólicas
+      </NavLink>
     </nav>
   );
 }
