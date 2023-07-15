56d28f5bc3ba272208120f67007e6b3ba78cf359
TomazDimas <tomazdimas2001@gmail.com>
2023-07-15 11:00:43 -0300

    style: Estilização da barra de navegação

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,35 +12,37 @@ import './NavBar.css';
 
 function Navegation() {
   return (
-    <nav className="navbar-container">
-      <NavLink to="/">
-        <AiOutlineHome />
-        Home
-      </NavLink>
-      <NavLink to="/categories">
-        <AiOutlineAppstore />
-        Todas Categorias
-      </NavLink>
-      <NavLink to="/offers">
-        <AiOutlineTags />
-        Ofertas
-      </NavLink>
-      <NavLink to="/barbecue">
-        <GiBarbecue />
-        Kit Churrasco
-      </NavLink>
-      <NavLink to="/meats">
-        <TbMeat />
-        Açougue/ Aves/ Peixaria
-      </NavLink>
-      <NavLink to="/dairy">
-        <BiCheese />
-        Laticínios
-      </NavLink>
-      <NavLink to="/drinks">
-        <BiDrink />
-        Bebidas Alcoólicas
-      </NavLink>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <NavLink to="/" className="navbar-container__item">
+          <AiOutlineHome />
+          Home
+        </NavLink>
+        <NavLink to="/categories" className="navbar-container__item">
+          <AiOutlineAppstore />
+          Todas Categorias
+        </NavLink>
+        <NavLink to="/offers" className="navbar-container__item">
+          <AiOutlineTags />
+          Ofertas
+        </NavLink>
+        <NavLink to="/barbecue" className="navbar-container__item">
+          <GiBarbecue />
+          Kit Churrasco
+        </NavLink>
+        <NavLink to="/meats" className="navbar-container__item">
+          <TbMeat />
+          Açougue/ Aves/ Peixaria
+        </NavLink>
+        <NavLink to="/dairy" className="navbar-container__item">
+          <BiCheese />
+          Laticínios
+        </NavLink>
+        <NavLink to="/drinks" className="navbar-container__item">
+          <BiDrink />
+          Bebidas Alcoólicas
+        </NavLink>
+      </div>
     </nav>
   );
 }
