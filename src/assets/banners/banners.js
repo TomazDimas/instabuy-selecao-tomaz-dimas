67a6f1ef96dea50aba5899fc5bf1116cff45eb26
TomazDimas <tomazdimas2001@gmail.com>
2023-07-16 11:42:38 -0300

    feat: Desenvolvimento inicial swiper

---
+++ src/assets/banners/banners.js
@@ -0,0 +1,9 @@
+import banner3 from './banner3.jpeg';
+import banner4 from './banner4.jpeg';
+
+const banners = [
+  { id: 1, img: banner3 },
+  { id: 2, img: banner4 },
+];
+
+export default banners;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
+import banners from '../../assets/banners/banners';
 
 function Slider() {
-  return <div>Slider</div>;
+  console.log(banners);
+
+  return (
+    <div>
+      <Swiper
+        className="swiper"
+        slidesPerView={ 1 }
+        modules={ [Autoplay] }
+        autoplay={ { delay: 1000 } }
+      >
+        {banners.map((banner) => (
+          <SwiperSlide key={ banner.id }>
+            <img src={ banner.img } alt="banner" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
 export default Slider;
