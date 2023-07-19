97bc9ff30c4e7da9e4bca6f145ef52eea54cdc43
TomazDimas <tomazdimas2001@gmail.com>
2023-07-19 15:32:17 -0300

    feat: Implementação da requisição /layout

--- src/pages/HomeScreem/HomeScreen.jsx
+++ src/pages/HomeScreem/HomeScreen.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import ProductBar from '../../components/ProductBar/ProductBar';
 import Slider from '../../components/Slider/Slider';
 import './HomeScreen.css';
 
 function HomeScreen() {
+  const [promoProducts, setPromoProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const baseUrl = 'https://api.instabuy.com.br/apiv3/';
+    try {
+      const response = await axios.get(`${baseUrl}layout`, {
+        params: { subdomain: 'supermercado' },
+      });
+      const { data } = response.data;
+      setPromoProducts(data.promo);
+      setProducts(data.collection_items);
+      console.log(data);
+      console.log(products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="home-container">
       <Slider />
-      <ProductBar />
+      <ProductBar products={ promoProducts } />
     </div>
   );
 }
