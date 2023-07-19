c6be4d56d1d682963d2ba914fc4e822691895fe2
TomazDimas <tomazdimas2001@gmail.com>
2023-07-18 22:07:47 -0300

    feat: Criação do componente barra de produto

---
+++ src/components/ProductBar/ProductBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ProductBar.css';
+
+function ProductBar() {
+  return (
+    <div className="product-bar-container">
+      <h1>Título</h1>
+      <div className="product-container">
+        <h2>item</h2>
+        <h2>item</h2>
+        <h2>item</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ProductBar;
