b319e5de944d84dca950f1b4babbccbbd3961f66
TomazDimas <tomazdimas2001@gmail.com>
2023-07-19 15:31:52 -0300

    style: Importação da estilização do swiper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+/* eslint-disable import/no-unresolved */
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import HomeScreen from './pages/HomeScreem/HomeScreen';
 import ProductScreen from './pages/ProductScreen/ProductScreen';
 import './App.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 function App() {
   return (
