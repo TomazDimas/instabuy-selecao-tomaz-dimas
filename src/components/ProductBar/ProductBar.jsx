f24d3110529f07e788f16f248af9f8e66ea463f2
TomazDimas <tomazdimas2001@gmail.com>
2023-07-19 15:31:01 -0300

    feat: Deseevolvimento da barra de produto

--- src/components/ProductBar/ProductBar.jsx
+++ src/components/ProductBar/ProductBar.jsx
@@ -1,18 +1,39 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
+import propTypes from 'prop-types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import { BsLightningCharge } from 'react-icons/bs';
 import ProductCell from '../ProductCell/ProductCell';
 import './ProductBar.css';
 
-function ProductBar() {
+function ProductBar({ products }) {
   return (
     <div className="product-bar-container">
-      <h1>Título</h1>
-      <div className="product-container">
-        <ProductCell />
-        <ProductCell />
-        <ProductCell />
-      </div>
+      <h1 className="product-bar__title">
+        <BsLightningCharge className="product-bar__icon" />
+        Ofertas
+      </h1>
+      <Swiper
+        pagination={ { clickable: true } }
+        navigation
+        spaceBetween={ 50 }
+        modules={ [Navigation] }
+        slidesPerView={ 6 }
+        className="product-container"
+      >
+        {products.map((product) => (
+          <SwiperSlide key={ product.id }>
+            <ProductCell data={ product } />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
 
 export default ProductBar;
+
+ProductBar.propTypes = {
+  data: propTypes.shape({}),
+}.isRequired;
