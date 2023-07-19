44a872427ae37fa1e0961499e13b53792791bd8f
TomazDimas <tomazdimas2001@gmail.com>
2023-07-18 22:05:29 -0300

    feat: Desenvolvimento do swiper

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,24 +1,29 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
+import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper';
 import banners from '../../assets/banners/banners';
+import './Slider.css';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
 function Slider() {
-  console.log(banners);
-
   return (
-    <div>
+    <div className="swiper-container">
       <Swiper
-        className="swiper"
-        slidesPerView={ 1 }
         modules={ [Autoplay] }
-        autoplay={ { delay: 1000 } }
+        autoplay={ { delay: 5000 } }
+        slidesPerView={ 1 }
       >
-        {banners.map((banner) => (
-          <SwiperSlide key={ banner.id }>
-            <img src={ banner.img } alt="banner" />
+        { banners.map((item) => (
+          <SwiperSlide key={ item.id }>
+            <img
+              src={ item.img }
+              alt="banner"
+              className="swiper-slide__image"
+            />
           </SwiperSlide>
-        ))}
+        )) }
       </Swiper>
     </div>
   );
