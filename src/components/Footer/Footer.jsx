481e18fb6bfb7809c33621a2c75d4c7c0dc0b7c4
TomazDimas <tomazdimas2001@gmail.com>
2023-07-28 19:29:31 -0300

    feat: Criação do rodapé

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <h2>
+          Asa Norte, Campus Universitário Darcy Ribeiro Bloco A PMU I Sala AT
+          08/03 - AC UNB, Edificio CDT, Brasília - DF
+        </h2>
+        <h2>© 2023 Todos os Direitos Reservados</h2>
+        <h2>
+          Fotos ilustrativas. Compras podem ser canceladas em caso de suspeita
+          de fraude. O valor total de sua compra poderá ser alterado para mais
+          ou para menos por conta dos produtos de peso variável. Preços, ofertas
+          e condições exclusivos para internet e válidos durante o dia de hoje,
+          podendo sofrer alterações sem prévia notificação. Em caso de
+          divergência de valores no site, o valor válido é o do carrinho de
+          compras. Venda sujeita à disponibilidade de estoque no dia da entrega.
+          No caso de faltar algum produto, este não será entregue e o valor
+          correspondente não será cobrado. Proibida a venda de bebidas
+          alcoólicas para menores de 18 anos. Beba com moderação.
+        </h2>
+        <h2>Desenvolvido por Tomaz Dimas</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
