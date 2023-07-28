b70b9e6cbb169dae91d30c22b4a6a8efb94b8d4d
TomazDimas <tomazdimas2001@gmail.com>
2023-07-28 19:31:14 -0300

    fix: Correções na tela inicial

--- src/pages/HomeScreem/HomeScreen.jsx
+++ src/pages/HomeScreem/HomeScreen.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Loading from '../../components/Loading/Loading';
 import ProductBar from '../../components/ProductBar/ProductBar';
 import PromoProductBar from '../../components/ProductBar/PromoProductBar';
 import Slider from '../../components/Slider/Slider';
@@ -9,16 +10,18 @@ import './HomeScreen.css';
 function HomeScreen() {
   const [promoProducts, setPromoProducts] = useState([]);
   const [products, setProducts] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const getProducts = async () => {
     const baseUrl = 'https://api.instabuy.com.br/apiv3/';
     try {
       const response = await axios.get(`${baseUrl}layout`, {
-        params: { subdomain: 'supermercado' },
+        params: { subdomain: 'supermercado', github_id: 'TomazDimas' },
       });
       const { data } = response.data;
       setPromoProducts(data.promo);
       setProducts(data.collection_items);
+      setIsLoaded(true);
     } catch (error) {
       console.log(error);
     }
@@ -29,13 +32,15 @@ function HomeScreen() {
   }, []);
 
   return (
-    <div className="home-container">
-      <Slider />
-      <PromoProductBar products={ promoProducts } />
-      { products.map((collection) => (
-        <ProductBar key={ collection.id } data={ collection } />
-      )) }
-    </div>
+    (!isLoaded && <Loading />) || (
+      <div className="home-container">
+        <Slider />
+        <PromoProductBar products={ promoProducts } />
+        {products.map((collection) => (
+          <ProductBar key={ collection.id } data={ collection } />
+        ))}
+      </div>
+    )
   );
 }
 
