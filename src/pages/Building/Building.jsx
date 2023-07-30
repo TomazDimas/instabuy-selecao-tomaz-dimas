30ec952d9d4b0d397111560125489297b6a786a4
TomazDimas <tomazdimas2001@gmail.com>
2023-07-30 20:09:10 -0300

    feat: Criação da tela de building

---
+++ src/pages/Building/Building.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MdBuild } from 'react-icons/md';
+import './Building.css';
+
+function BuildingScreen() {
+  return (
+    <div className="building-container">
+      <h1>Página em construção...</h1>
+      <MdBuild className="building__icon" />
+    </div>
+  );
+}
+
+export default BuildingScreen;
