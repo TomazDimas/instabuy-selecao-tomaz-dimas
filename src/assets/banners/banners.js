f43c815579f666c9ce08e7076499d1d0a9d3e8dc
TomazDimas <tomazdimas2001@gmail.com>
2023-07-30 20:21:59 -0300

    fix: Remoção das imagens baixadas de banner

--- src/assets/banners/banners.js
+++
@@ -1,9 +0,0 @@
-import banner3 from './banner3.jpeg';
-import banner4 from './banner4.jpeg';
-
-const banners = [
-  { id: 1, img: banner3 },
-  { id: 2, img: banner4 },
-];
-
-export default banners;
