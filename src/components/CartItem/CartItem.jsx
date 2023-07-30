54267365fadf6d5952b4d4a5dc233863df1f32e2
TomazDimas <tomazdimas2001@gmail.com>
2023-07-30 20:07:09 -0300

    fix: Correções no componente de elemento do carrinho

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -4,20 +4,14 @@ import { BsTrash } from 'react-icons/bs';
 import formatCurrency from '../../utils/formatCurrency';
 import './CartItem.css';
 import CartContext from '../../context/CartContext';
+import removeProductById from '../../utils/removeProductById';
 
 function CartItem({ productData }) {
   const { cartProducts, setCartProducts } = useContext(CartContext);
 
-  const handleRemoveProduct = () => {
-    const currentCart = [...cartProducts];
-    const productIndex = currentCart
-      .findIndex((product) => product.id === productData.id);
-    currentCart.splice(productIndex, 1);
-    setCartProducts(currentCart);
-  };
-
   const getProductAmmount = () => {
-    return cartProducts.find((product) => product.id === productData.id).ammount;
+    return cartProducts.find((product) => product.id === productData.id)
+      .ammount;
   };
 
   return (
@@ -28,15 +22,18 @@ function CartItem({ productData }) {
           src={ `https://assets.instabuy.com.br/ib.item.image.small/s-${productData.images[0]}` }
         />
         <h2 className="cart-item__title">{productData.name}</h2>
-        <input
-          type="number"
-          value={ getProductAmmount() }
-        />
+        <input type="number" value={ getProductAmmount() } />
         <h2>
-          {formatCurrency(productData.prices[0].price * getProductAmmount(), 'BRL')}
+          {formatCurrency(
+            productData.prices[0].price * getProductAmmount(),
+            'BRL',
+          )}
         </h2>
       </div>
-      <button onClick={ handleRemoveProduct } className="cart-item-remove-container">
+      <button
+        onClick={ () => removeProductById(productData.id, cartProducts, setCartProducts) }
+        className="cart-item-remove-container"
+      >
         <BsTrash className="cart-item-remove__icon" />
         <h3 className="cart-item-remove__text">Remover</h3>
       </button>
